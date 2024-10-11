89ef7b2f6ba9fbc6fce8ab5da0da3616d45ac5ab
Matt Daniels <matt@techtoniq.com>
2024-10-11 17:10:00 +0100

    Added a more thorough test

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Diamond Kata in C#
 
 One possible answer to this [Code dojo](https://github.com/davidwhitney/CodeDojos/tree/master/Diamond%20Kata)
+
+## Notes
+
+The unit testing approach is something that should be discussed at tech breakdown / prior to development, 
+as unit testing sequences (which is what this challenge is really about) can result in tests duplicating 
+the logic of the subject under test - something which should be avoided.
--- Test/DiamondKata.UnitTest/TextDiamondGeneratorTests.cs
+++ Test/DiamondKata.UnitTest/TextDiamondGeneratorTests.cs
@@ -46,7 +46,7 @@ namespace DiamondKata.UnitTest
                                           "C   C", 
                                           " B B ", 
                                           "  A  " })]
-            public void When_Valid_Input_Then_Diamond_Generated_Correctly(char input, string[] expectedOutput)
+            public void When_Valid_Input_Then_Diamond_Generated_Correctly_v1(char input, string[] expectedOutput)
             {
                 // Arrange
 
@@ -62,6 +62,86 @@ namespace DiamondKata.UnitTest
                 result.Value.Should().NotBeEmpty()
                     .And.HaveCount(expectedOutput.Length)
                     .And.ContainInOrder(expectedOutput);
+            }
+
+
+            private static char[] _validTestInputs = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+            [TestCaseSource(nameof(_validTestInputs))]
+            public void When_Valid_Input_Then_Diamond_Should_Contain_Correct_Number_Of_Lines(char input)
+            {
+                // Arrange
+
+                var generator = new TextDiamondGenerator();
+                var expectedLineCount = (input - 'A') * 2 + 1;
+
+                // Act
+
+                var result = generator.GenerateDiamond(input);
+
+                //Assert
+
+                result.IsSuccess.Should().BeTrue();
+                result.Value.Should().NotBeEmpty()
+                    .And.HaveCount(expectedLineCount);
+            }
+
+            [TestCaseSource(nameof(_validTestInputs))]
+            public void When_Valid_Input_Then_Diamond_Generated_Correctly_v2(char input)
+            {
+                // Arrange
+
+                var generator = new TextDiamondGenerator();
+                
+                // Act
+
+                var result = generator.GenerateDiamond(input);
+
+                //Assert
+
+                result.IsSuccess.Should().BeTrue();
+
+                int lineIndex = 0;
+                var lines = result.Value.ToList();
+                for(char lineLetter = 'A'; lineLetter < input; lineLetter++)
+                {
+                    var expected = ExpectedLineForLetter(input, lineLetter);
+                    lines[lineIndex++].Should().Be(expected);
+                }
+                for (char lineLetter = input; lineLetter >= 'A'; lineLetter--)
+                {
+                    var expected = ExpectedLineForLetter(input, lineLetter);
+                    lines[lineIndex++].Should().Be(expected);
+                }
+            }
+
+
+            /// <summary>
+            /// For a given input letter, what is the expected text output text for the line with the specified letter on it?
+            /// </summary>
+            /// <remarks>The danger here is that we start coding logic in the tests which duplicates the logic of the subject under test.</remarks>
+            /// <param name="inputLetter">Input letter</param>
+            /// <param name="lineLetter">Line to return the expected text for</param>
+            /// <returns>Expected text</returns>
+            private string ExpectedLineForLetter(char inputLetter, char lineLetter)
+            {
+                if (inputLetter == 'A')
+                    return "A";
+
+                var outsideSpaceCount = inputLetter - lineLetter;
+
+                if (lineLetter == 'A')
+                {
+                    return $"{new string(' ', outsideSpaceCount)}{lineLetter}{new string(' ', outsideSpaceCount)}";
+                }
+                else
+                {
+                    var lineAlphabeticPosition = (lineLetter - 'A') + 1;
+                    var insideSpaceCount = ((lineAlphabeticPosition - 1) * 2) - 1;
+
+                    return $"{new string(' ', outsideSpaceCount)}{lineLetter}{new string(' ', insideSpaceCount)}{lineLetter}{new string(' ', outsideSpaceCount)}";
+                }
             }
         }
     }
